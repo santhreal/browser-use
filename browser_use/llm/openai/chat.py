0e81fd84cd0d1e28b5380ee122ad69c77c088fc2
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-09-09 22:38:47 -0700

    Degrease reasoning_effort

--- browser_use/llm/openai/chat.py
+++ browser_use/llm/openai/chat.py
@@ -36,7 +36,7 @@ class ChatOpenAI(BaseChatModel):
 	# Model params
 	temperature: float | None = 0.2
 	frequency_penalty: float | None = 0.3  # this avoids infinite generation of \t for models like 4.1-mini
-	reasoning_effort: ReasoningEffort = 'minimal'
+	reasoning_effort: ReasoningEffort = 'low'
 	seed: int | None = None
 	service_tier: Literal['auto', 'default', 'flex', 'priority', 'scale'] | None = None
 	top_p: float | None = None
@@ -182,7 +182,11 @@ class ChatOpenAI(BaseChatModel):
 				model_params['service_tier'] = self.service_tier
 
 			if self.reasoning_models and any(str(m).lower() in str(self.model).lower() for m in self.reasoning_models):
-				model_params['reasoning_effort'] = self.reasoning_effort
+				if 'gpt-5-mini' in self.model:
+					model_params['reasoning_effort'] = 'minimal'
+				else:
+					model_params['reasoning_effort'] = self.reasoning_effort
+
 				del model_params['temperature']
 				del model_params['frequency_penalty']
 
