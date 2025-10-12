99c2b2b6c4dcfcb75e139de3d5a3f82589b411d3
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-10-12 12:51:42 -0700

    Include scroll instruction

--- browser_use/agent/prompts.py
+++ browser_use/agent/prompts.py
@@ -221,14 +221,14 @@ class AgentMessagePrompt:
 				if self.browser_state.page_info:
 					pi = self.browser_state.page_info
 					pages_above = pi.pixels_above / pi.viewport_height if pi.viewport_height > 0 else 0
-					elements_text = f'... {pages_above:.1f} pages above ...\n{elements_text}'
+					elements_text = f'... {pages_above:.1f} pages above, scroll/extract to see more ...\n{elements_text}'
 			else:
 				elements_text = f'[Start of page]\n{elements_text}'
 			if has_content_below:
 				if self.browser_state.page_info:
 					pi = self.browser_state.page_info
 					pages_below = pi.pixels_below / pi.viewport_height if pi.viewport_height > 0 else 0
-					elements_text = f'{elements_text}\n...{pages_below:.1f}pages below'
+					elements_text = f'{elements_text}\n...{pages_below:.1f}pages below, scroll/extract to see more'
 			else:
 				elements_text = f'{elements_text}\n[End of page]'
 		else:
